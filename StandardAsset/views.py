0bf25427ae178454874d1c3a49a2fd2beb7fc902
Izaxson <67207370+Izaxson@users.noreply.github.com>
2023-08-13 07:02:31 +0300

    requirement.txt

--- StandardAsset/views.py
+++ StandardAsset/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-# number_of_years=(todays date- Date_of_Delivery)/365--date diffrence conveeted to years
+# number_of_years=(todays date- Date_of_Delivery)/365--date diffrence converted to years
 # accumulated_depreciation = depreciation_rate * number_of_years
 # net_book_value = purchase_amount - accumulated_depreciation
